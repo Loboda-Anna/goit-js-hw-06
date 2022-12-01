3ec51affa538a7d505a5b039eb953376af576d2a
Loboda-Anna <107415190+Loboda-Anna@users.noreply.github.com>
2022-12-01 20:56:31 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,17 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+for (const ingredient of ingredients) {
+  const ingredientsList = document.createElement("li");
+  ingredientsList.textContent = [ingredient];
+  ingredientsList.classList.add("item");
+
+  const ingredientsEl = document.querySelector("#ingredients");
+  ingredientsEl.append(ingredientsList);
+}
