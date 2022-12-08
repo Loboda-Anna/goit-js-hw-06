dd9c347091f0a74aedf91bca94b9d8193379f0f6
Loboda-Anna <107415190+Loboda-Anna@users.noreply.github.com>
2022-12-08 21:57:15 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,13 @@
+const changeColorBtn = document.querySelector(".change-color");
+const colorHexEl = document.querySelector(".color");
+
+changeColorBtn.addEventListener("click", onChangeColorBtnClick);
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+function onChangeColorBtnClick() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorHexEl.textContent = document.body.style.backgroundColor;
 }
