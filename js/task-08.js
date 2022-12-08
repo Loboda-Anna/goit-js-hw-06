347800fb9bd6d6931d427ec3e2b76354905a47c8
Loboda-Anna <107415190+Loboda-Anna@users.noreply.github.com>
2022-12-08 18:55:03 +0200

    Update task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,11 @@
+const formEl = document.querySelector(".login-form");
 
+formEl.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+  event.preventDefault();
+
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
+}
