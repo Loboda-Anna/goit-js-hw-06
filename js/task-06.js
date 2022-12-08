4d51bd51ce6c80271d5ac668901e0d9425ad0067
Loboda-Anna <107415190+Loboda-Anna@users.noreply.github.com>
2022-12-08 14:04:01 +0200

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -4,6 +4,14 @@ inputEl.addEventListener("blur", onInputBlur);
 
 function onInputBlur() {
   inputEl.value.length === Number(inputEl.dataset.length)
-    ? inputEl.classList.replace("invalid", "valid")
+    ? inputEl.classList.add("valid")
     : inputEl.classList.add("invalid");
+
+  if (
+    inputEl.value.length === Number(inputEl.dataset.length) &&
+    inputEl.classList.contains("invalid")
+  ) {
+    inputEl.classList.replace("invalid", "valid");
+  }
 }
+
