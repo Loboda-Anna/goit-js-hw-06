5f755608541a4cf52ae993d0ae0e5baeb007dd10
Loboda-Anna <107415190+Loboda-Anna@users.noreply.github.com>
2022-12-09 12:15:49 +0200

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -1,3 +1,25 @@
+const boxesNumberEl = document.querySelector("#controls").firstElementChild;
+const createBtn = document.querySelector("button[data-create]");
+const destroyBtn = document.querySelector("button[data-destroy]");
+const mainBoxEl = document.querySelector("#boxes");
+
+// createBtn.addEventListener("click", onCreateClick);
+// destroyBtn.addEventListener("click", onDestroyClick);
+
+function createBoxes(amount) {
+  for (let i = 0, p = 30; i < amount, p < 1200; i += 1, p += 10) {
+    const newBoxEl = document.createElement("div");
+
+    newBoxEl.style.width = `${p}px`;
+    newBoxEl.style.height = `${p}px`;
+    newBoxEl.style.backgroundColor = getRandomHexColor();
+
+    mainBoxEl.appendChild(newBoxEl);
+  }
+}
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+console.log(createBoxes(3));
